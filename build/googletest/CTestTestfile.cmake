7477a174ecf914092e0af76211ede944b04e3735
Akim <egorov.akim@mail.ru>
2022-04-16 07:40:05 +0300

    first

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/egoro/Documents/GitHub/UML/googletest
-# Build directory: /mnt/c/Users/egoro/Documents/GitHub/UML/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
