4cb1a68988c12eb54d707e5bc5492ce6dfd146c7
Vladislav <sdvlad75@gmail.com>
2022-05-14 11:15:28 +0300

    removed garbage

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/egoro/Documents/GitHub/UML/test
-# Build directory: /mnt/c/Users/egoro/Documents/GitHub/UML/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(utils_tests "/mnt/c/Users/egoro/Documents/GitHub/UML/build/test/utils_tests")
-set_tests_properties(utils_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/egoro/Documents/GitHub/UML/test/CMakeLists.txt;20;add_test;/mnt/c/Users/egoro/Documents/GitHub/UML/test/CMakeLists.txt;0;")
