63775ce7f2e296ec657316358c651282692a4fc8
vBykoff <v.bykov.2002@gmail.com>
2022-05-27 13:07:28 +0300

    //

--- source/main.cpp
+++ source/main.cpp
@@ -3,13 +3,15 @@
 #include "RecSys.h"
 #include "Dataset.h"
 
+
+
 int main() {
 
   torch::Tensor interactions = torch::rand({3,3});
   Dataset data(interactions);
   RecSys<Net> recommender(data);
   double loss = recommender.fit();
-  recommender.u2i_predict(id_dock, 5);
+//  recommender.u2i_predict(id_dock, 5);
   return 0;
 }
 
